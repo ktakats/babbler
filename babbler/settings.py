928e908138a5f5417affb62f4a74db514728085e
ktakats <ktakats@gmail.com>
2018-02-11 17:42:19 +0100

    add dj-database-url

--- babbler/settings.py
+++ babbler/settings.py
@@ -106,6 +106,8 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
